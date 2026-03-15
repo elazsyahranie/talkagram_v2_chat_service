cfd94cc96211b9274541a2c4555ba32b15d9ce5a
elazsyahranie <elazaribrahims@gmail.com>
2026-03-15 23:41:35 +0700

    deleted fetchExternalApi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,8 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { finalResultType } from './external-api.interface';
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('/fetch')
-  async fetchExternalApi(
-    @Query() query: { results?: number; page?: number },
-  ): Promise<finalResultType[]> {
-    const result = await this.appService.fetchExternalApi(
-      query.results,
-      query.page,
-    );
-    return result;
-  }
-
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,50 +1,8 @@
-import { Injectable, BadGatewayException } from '@nestjs/common';
-import { externalApiResponse, finalResultType } from './external-api.interface';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
-  }
-
-  async fetchExternalApi(
-    results?: number,
-    page?: number,
-  ): Promise<finalResultType[]> {
-    results = results ? results : 1;
-    page = page ? page : 1;
-
-    const res = await fetch(
-      `https://randomuser.me/api?results=${results}&page=${page}`,
-      {
-        cache: 'no-store',
-      },
-    );
-    if (!res.ok) {
-      throw new BadGatewayException(502, 'Failed to fetch data');
-    }
-
-    const fetchedData = await res.json();
-
-    let finalResult: finalResultType[] = [];
-    if (fetchedData) {
-      fetchedData.results.map((obj: externalApiResponse) => {
-        finalResult.push({
-          name: `${obj.name?.title}, ${obj.name?.first} ${obj.name?.last}`,
-          location: `${String(obj.location?.postcode ?? '')}, ${obj.location?.city}, ${obj.location?.state}, ${obj.location?.country}`,
-          email: `${obj.email}`,
-          age: obj.dob?.age ?? 0,
-          phone: `${obj.phone}`,
-          cell: `${obj.cell}`,
-          picture: [
-            `${obj.picture?.large}`,
-            `${obj.picture?.medium}`,
-            `${obj.picture?.thumbnail}`,
-          ],
-        });
-      });
-    }
-
-    return finalResult;
   }
 }
